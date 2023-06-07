5e990e297411b1a605b63420adb27a2a4baa27e5
jm <slzlxn@naver.com>
2023-06-07 17:25:42 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (e) => {
+    //form은 전송 후에 refresh(새로고침) 된다
+    e.preventDefault();
+
+    console.log(username, password);
+  };
+
+  //onChang는 input의 값이 바꿀때마다 실행하는 함수
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        {/*<form onSubmit={onSubmit}> -> form에는 onSubmit 필수*****  */}
+        <input
+          placeholder="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <br />
+        <input
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />{" "}
+        <br />
+        <button type="submit">Update</button>
+      </form>
+    </>
   );
 }
 
